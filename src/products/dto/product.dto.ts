c89e48e950631b8899082b91bcd6328632dd338b
Dima <d.didkivskyi@gmail.com>
2023-03-31 15:58:25 +0300

    Created simple logic for products

---
+++ src/products/dto/product.dto.ts
@@ -0,0 +1,7 @@
+export interface ProductDto {
+  name: string;
+  category: string;
+  characteristics: string;
+  description: string;
+  price: number;
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { ProductDto } from './dto/product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,5 +17,25 @@ export class ProductsController {
   @Get()
   async findProducts() {
     return await this.productsService.findProducts();
+  }
+
+  @Get('/:id')
+  async findProduct(@Param('id') id: string) {
+    return await this.productsService.findProduct(id);
+  }
+
+  @Post()
+  async createProduct(@Body() productDto: ProductDto) {
+    return await this.productsService.createProduct(productDto);
+  }
+
+  @Patch('/:id')
+  async updateProduct(@Param('id') id: string, @Body() productDto: ProductDto) {
+    return await this.productsService.updateProduct(id, productDto);
+  }
+
+  @Delete('/:id')
+  async deleteProduct(@Param('id') id: string) {
+    return await this.productsService.deleteProduct(id);
   }
 }
